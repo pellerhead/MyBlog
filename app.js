4774f1b96eadb182ce2986056363ac81d10bfec0
Greg Nelson <nelsongreg35@gmail.com>
2020-10-07 22:48:12 -0500

    Added partials
    Used a for loop and a forEach loop for an array

--- app.js
+++ app.js
@@ -17,18 +17,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
+// The "Home" route or the "Root"
+app.get("/", function(req, res) {
+  res.render("home", {startingContent: homeStartingContent,
+                      posts: posts });
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
 
+  posts.push(post);
+  res.redirect("/");
 
-
-
-
-
-
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
