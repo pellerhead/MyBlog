f96779e78a819a5ec5bd58b98e92bc2048df2812
Greg Nelson <nelsongreg35@gmail.com>
2020-10-08 14:10:38 -0500

    Adding the use of Lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -45,6 +46,23 @@ app.post("/compose", function(req, res) {
 
   posts.push(post);
   res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res) {
+  //res.render("posts");
+
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === _.lowerCase(requestedTitle)) {
+      console.log("Found: " + storedTitle);
+    } else {
+      console.log("NOT FOUND: " + requestedTitle);
+    }
+  });
 
 });
 
